496e2ce00626f717c764edb332664c77daa95c76
lux_car_parfum <lolyura540@gmail.com>
2026-02-24 20:25:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ envelope.addEventListener("click", () => {
 });
 
 function createHearts() {
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 35; i++) {
     const heart = document.createElement("div");
     heart.className = "heart";
     heart.innerHTML = "❤️";
     heart.style.left = Math.random() * 100 + "vw";
-    heart.style.animationDuration = (Math.random() * 3 + 2) + "s";
+    heart.style.fontSize = (Math.random() * 14 + 16) + "px";
+    heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
     document.body.appendChild(heart);
 
-    setTimeout(() => heart.remove(), 5000);
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
   }
 }
